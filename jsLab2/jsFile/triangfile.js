32227c3788d52f6213ed98ac0dfe2186edc06347
AllRaos <rusl.chaban@gmail.com>
2025-03-11 00:59:56 +0200

    fixLab2

--- jsLab2/jsFile/triangfile.js
+++ jsLab2/jsFile/triangfile.js
@@ -10,9 +10,9 @@ console.log("Triangular objects demonstration:");
 const myTriangle1 = Triangular(); 
 console.log("Triangle 1 def:", myTriangle1);
 
-const triangle2 = Triangular({ a: 6, b: 16, c: 17 });
+const myTriangle2 = Triangular({ a: 6, b: 16, c: 17 });
 console.log("Triangle 2:", myTriangle2);
 
-const triangle3 = Triangular({ a: 7, b: 24, c: 25 });
+const myTriangle3 = Triangular({ a: 7, b: 24, c: 25 });
 console.log("Triangle 3:", myTriangle3);
 console.log("\n");
