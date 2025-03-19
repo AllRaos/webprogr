1af7a38040a44784052ea831b656c0211bac2e3e
AllRaos <rusl.chaban@gmail.com>
2025-03-19 20:41:50 +0200

    lab3relise

--- jsLab3/jsFile/script.js
+++ jsLab3/jsFile/script.js
@@ -8,4 +8,25 @@
         speakHello(names[i]);
       }
     }
+    console.log("If ASCII sum of vowel letters > 200 say Hello else say Good Bye.");
+    function getVowelsAsciiSum(name) {
+      let vowels = ["a", "e", "i", "o", "u"];
+      let sum = 0;
+      for (let i = 0; i < name.length; i++) {
+        let char = name.charAt(i).toLowerCase();
+        if (vowels.includes(char)) {
+          sum += char.charCodeAt(0); 
+        }
+      }
+      return sum;
+    }
+  
+    for (let i = 0; i < names.length; i++) {
+      let asciiSum = getVowelsAsciiSum(names[i]);
+      if (asciiSum > 200) {
+        speakHello(names[i]); 
+      } else {
+        speakGoodBye(names[i]); 
+      }
+    }
   })();
